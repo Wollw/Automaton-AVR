73f60fc4b3b3be636e90a60951fa74e41ddaca11
= <david.e.shere@gmail.com>
2011-11-25 16:12:00 -0800

    created gEDA schematic for 4-register board w/o switches

--- src/config.h
+++ src/config.h
@@ -1,38 +1,34 @@
-// 3x3 grid
-
-// Include once
 #pragma once
-#ifndef CONFIGED
-#define CONFIGED
+#ifndef CONFIGURED
+#define CONFIGURED
 
 //#define CONFIG_USE_SERIAL
 //#define	CONFIG_USE_SWITCHES_FOR_SETTINGS
-#define CONFIG_RULES_SURV	0b1100
-#define CONFIG_RULES_BIRTH	0b1000
-#define CONFIG_INITIAL_STATE	0b000111000
-
-// Time each turn lasts (the amount of time a unique state of the
-// automaton is displayed)
-#define CONFIG_DELAY_MS 1000
-
-/*
- *	An array of neighboring cells for each cell.
- *	Each index is a cell id so that Cell ID #0 has the neighbor
- *	value 0b000011010.  Each bit refers to another cell's ID so that
- *	the first cell (Cell ID #0) with the aforementioned neighbor value of
- *	0b000011010 translates to being neighbors with Cell ID #1, #3, and #4
- *	because bits 1, 3, and 4 are set.
-*/
+#define	CONFIG_RULES_SURV		0b0
+#define	CONFIG_RULES_BIRTH		0b0
+#define	CONFIG_INITIAL_STATE	0b0
+#define CONFIG_DELAY_MS 500
 #define CONFIG_NEIGHBORS \
     {                \
-        0b000011010, \
-        0b000111101, \
-        0b000110010, \
-        0b011010011, \
-        0b111101111, \
-        0b110010110, \
-        0b010011000, \
-        0b101111000, \
-        0b010110000, \
-    }
+		0b0000000000001111110,	\
+		0b0000000001111000101,	\
+		0b0000000101001011011,	\
+		0b0000011100000010101,	\
+		0b0001010000001101101,	\
+		0b0111000000001010001,	\
+		0b0100000000010110111,	\
+		0b0000000011101000010,	\
+		0b0000000011010000010,	\
+		0b0000000010110000110,	\
+		0b0000000001110000000,	\
+		0b0000111000000001100,	\
+		0b0000110100000001000,	\
+		0b0000101100000011000,	\
+		0b0000011100000000000,	\
+		0b1110000000000110000,	\
+		0b1101000000000100000,	\
+		0b1011000000001100000,	\
+		0b0111000000000000000	\
+    }	
+
 #endif
